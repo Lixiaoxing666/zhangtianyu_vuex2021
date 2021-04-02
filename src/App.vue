e865c2fa007123b07bf612665b19e9bbf2a9a08b
tianyucoder <zhangtianyu51177@sina.com>
2021-04-02 09:06:32 +0800

    更新

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
+/* 
+		Vue中组件名称推荐使用方案：
+				组件名文件名、组件引入名、组件标签名，均首字母大写；
+				如果组件名和原生html元素同名了，组件标签千万不要首字母小写；
+ */
 <template>
 	<div class="demo">
 		<h2>我是App组件</h2>
 		<h3>{{msg}}</h3>
 		<Hello/>
 		<hr/>
-		<Test/>
+		<test/>
 	</div>
 </template>
 
