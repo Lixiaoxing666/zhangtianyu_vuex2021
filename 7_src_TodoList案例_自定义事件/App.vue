5298909e7a6898fdcf098d9615b50da1e0385c20
tianyucoder <zhangtianyu51177@sina.com>
2021-04-02 17:11:29 +0800

    更新

---
+++ 7_src_TodoList案例_自定义事件/App.vue
@@ -0,0 +1,112 @@
+<template>
+	<div class="todo-container">
+    <div class="todo-wrap">
+      <Header @addtodo="addTodo"/>
+      <List v-show="todos.length" :todos="todos" :deleteTodo="deleteTodo" :updateTodo="updateTodo"/>
+      <Footer v-show="todos.length" :todos="todos" :updateAll="updateAll" :clearAllDone="clearAllDone"/>
+    </div>
+  </div>
+</template>
+
+<script>
+	import Header from './components/Header'
+	import List from './components/List'
+	import Footer from './components/Footer'
+
+	export default {
+		name:'App',
+		components:{Header,List,Footer},
+		data(){
+			return {
+				todos:JSON.parse(localStorage.getItem('todos')) || []
+			}
+		},
+		methods:{
+			//添加一个
+			addTodo(todoObj){
+				this.todos.unshift(todoObj)
+			},
+			//删除一个
+			deleteTodo(id){
+				this.todos = this.todos.filter((t)=>{
+					return t.id !== id
+				})
+			},
+			//更新一个
+			updateTodo(id,done){
+				this.todos.forEach((todoObj)=>{
+					if(todoObj.id === id) todoObj.done = done
+				})
+
+				//原来在react中我们如下操作，因为react规定：不能通过非setState的形式修改源数据
+				/* const x = this.todos.map((todoObj)=>{
+					if(todoObj.id === id) return {...todoObj,done}
+					return todoObj
+				})
+				this.todos = x */
+			},
+			//更新所有
+			updateAll(done){
+				this.todos.forEach((t)=>{
+					t.done = done
+				})
+			},
+			//清除所有已完成
+			clearAllDone(){
+				this.todos = this.todos.filter((t)=>{
+					return !t.done
+				})
+			}
+		},
+		watch:{
+			todos:{
+				deep:true, //开启深度监视
+				handler(value){
+					console.log('todos被改了')
+					localStorage.setItem('todos',JSON.stringify(value))
+				}
+			}
+		}
+	}
+</script>
+
+<style>
+	/*base*/
+	body {
+		background: #fff;
+	}
+	.btn {
+		display: inline-block;
+		padding: 4px 12px;
+		margin-bottom: 0;
+		font-size: 14px;
+		line-height: 20px;
+		text-align: center;
+		vertical-align: middle;
+		cursor: pointer;
+		box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
+		border-radius: 4px;
+	}
+	.btn-danger {
+		color: #fff;
+		background-color: #da4f49;
+		border: 1px solid #bd362f;
+	}
+	.btn-danger:hover {
+		color: #fff;
+		background-color: #bd362f;
+	}
+	.btn:focus {
+		outline: none;
+	}
+	.todo-container {
+		width: 600px;
+		margin: 0 auto;
+	}
+	.todo-container .todo-wrap {
+		padding: 10px;
+		border: 1px solid #ddd;
+		border-radius: 5px;
+	}
+
+</style>
---
+++ 7_src_TodoList案例_自定义事件/components/Item.vue
@@ -0,0 +1,88 @@
+/* 
+		关于props与watch
+				1.props是只读的，（Vue底层会监测你对props的修改，如果收到了以后，进行修改，就会报错）
+					备注：；Vue监测props修改时对于对象类型的数据，是判断地址值。
+				2.watch中配置的监视属性名，可以是vc或vm身上任何一个属性
+ */
+
+<template>
+	<li 
+		@mouseenter="isEnter = true" 
+		@mouseleave="isEnter = false" 
+		:class="{enter:isEnter}"
+	>
+		<label>
+			<!-- <input type="checkbox" v-model="todoObj.done"/> -->
+			<input type="checkbox" :checked="todoObj.done" @change="updateT(todoObj.id,$event)"/>
+			<span>{{todoObj.title}}</span>
+		</label>
+		<button 
+			class="btn btn-danger" 
+			@click="deleteT(todoObj.id)"
+			:style="{display:isEnter ? 'block' : 'none'}"
+		>删除</button>
+	</li>
+</template>
+
+<script>
+	export default {
+		name:'Item',
+		data(){
+			return {
+				isEnter:false
+			}
+		},
+		methods:{
+			//删除
+			deleteT(id){
+				if(confirm('确定删除？')){
+					this.deleteTodo(id)
+				}
+			},
+			//响应勾选 or 取消勾选
+			updateT(id,e){
+				this.updateTodo(id,e.target.checked)
+			}
+		},
+		props:{
+			todoObj:Object,
+			deleteTodo:Function,
+			updateTodo:Function
+		},
+	}
+</script>
+
+<style scoped>
+	/*item*/
+	li {
+		list-style: none;
+		height: 36px;
+		line-height: 36px;
+		padding: 0 5px;
+		border-bottom: 1px solid #ddd;
+	}
+	li label {
+		float: left;
+		cursor: pointer;
+	}
+	li label li input {
+		vertical-align: middle;
+		margin-right: 6px;
+		position: relative;
+		top: -1px;
+	}
+	li button {
+		float: right;
+		display: none;
+		margin-top: 3px;
+	}
+	li:before {
+		content: initial;
+	}
+	li:last-child {
+		border-bottom: none;
+	}
+	.enter{
+		background-color: #ddd;
+	}
+</style>
