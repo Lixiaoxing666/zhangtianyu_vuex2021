32b5b1bab357120cf00b0f96e3ca6a0246c224eb
tianyucoder <zhangtianyu51177@sina.com>
2021-04-06 10:16:54 +0800

    更新

---
+++ 8_src_全局事件总线/App.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="app">
+		<h2>我是App</h2>
+		<Demo1></Demo1>
+		<br/>
+		<Demo2></Demo2>
+	</div>
+</template>
+
+<script>
+	import Demo1 from './components/Demo1'
+	import Demo2 from './components/Demo2'
+	
+	export default {
+		name:'App',
+		components:{Demo1,Demo2},
+		mounted(){
+			console.log('App',this)
+		}
+	}
+</script>
+
+<style>
+	.app{
+		background-color: gray;
+		padding: 15px;
+	}	
+</style>
