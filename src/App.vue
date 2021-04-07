14b49d797c0f47bba89a80f16104ad4359e58196
tianyucoder <zhangtianyu51177@sina.com>
2021-04-07 18:50:42 +0800

    更新

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,17 @@
 <template>
-	<div>
-		<button @click="getStuInfo">点我获取学生信息</button>
-		<button @click="getCarInfo">点我获取汽车信息</button>
-	</div>
+	<el-row>
+		<el-button>默认按钮</el-button>
+		<el-button type="primary">主要按钮</el-button>
+		<el-button type="success">成功按钮</el-button>
+		<el-button type="info">信息按钮</el-button>
+		<el-button type="warning">警告按钮</el-button>
+		<el-button type="danger">危险按钮</el-button>
+		<el-input></el-input>
+	</el-row>
 </template>
 
 <script>
-	import axios from 'axios'
 	export default {
 		name:'App',
-		methods: {
-			getStuInfo(){
-				axios.get('http://localhost:8080/api1/students').then(
-					response => {console.log('成功了',response.data)},
-					error => {console.log('失败了',error.message)}
-				)
-			},
-			getCarInfo(){
-				axios.get('http://localhost:8080/api2/cars').then(
-					response => {console.log('成功了',response.data)},
-					error => {console.log('失败了',error.message)}
-				)
-			}
-		},
 	}
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
+// import ElementUI from 'element-ui';
+// import 'element-ui/lib/theme-chalk/index.css';
+import {Button,Input,Row} from 'element-ui';
 import App from './App'
 
 Vue.config.productionTip = false
+// Vue.use(ElementUI);
+Vue.component(Button.name, Button);
+Vue.component(Input.name, Input);
+Vue.component(Row.name, Row);
 
 new Vue({
 	el:'#app',
