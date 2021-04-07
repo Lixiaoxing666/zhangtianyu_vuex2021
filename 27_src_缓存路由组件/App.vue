6f6f51af4f41975aa25a18e115b3b2ae657ae296
tianyucoder <zhangtianyu51177@sina.com>
2021-04-07 17:02:05 +0800

    更新

---
+++ 27_src_缓存路由组件/App.vue
@@ -0,0 +1,38 @@
+<template>
+	<div>
+    <!-- <Header :a="'100'"/> -->
+    <!-- <Header :a="100"/> -->
+    <Header a="100"/>
+    <div class="row">
+      <div class="col-xs-2 col-xs-offset-2">
+        <div class="list-group">
+					<!-- 普通html中我们使用a标签进行页面的跳转 -->
+          <!-- <a class="list-group-item" href="./about.html">About</a>
+          <a class="list-group-item" href="./home.html">Home</a> -->
+
+					<!-- Vue中我们使用 <router-link> -->
+					<router-link class="list-group-item" active-class="active" to="/about">About</router-link>
+          <router-link class="list-group-item" replace active-class="active" to="/home">Home</router-link>
+        </div>
+      </div>
+      <div class="col-xs-6">
+        <div class="panel">
+          <div class="panel-body">
+						<router-view></router-view>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+	import Header from './components/Header'
+	export default {
+		name:'App',
+		components:{Header},
+		mounted(){
+			// console.log('App',this)
+		}
+	}
+</script>
---
+++ 27_src_缓存路由组件/pages/Detail.vue
@@ -0,0 +1,28 @@
+<template>
+	<ul>
+		<li>ID:{{id}}</li>
+		<li>TITLE:{{title}}</li>
+		<li>CONTENT:{{content}}</li>
+	</ul>
+</template>
+
+<script>
+	export default {
+		name:'Detail',
+		props:['id','title','content'],
+		// computed:{
+		// 	// id(){
+		// 	// 	return this.$route.params.id
+		// 	// },
+		// 	title(){
+		// 		return this.$route.query.title
+		// 	},
+		// 	content(){
+		// 		return this.$route.query.content
+		// 	}
+		// },
+		mounted(){
+			console.log('Detail',this)
+		}
+	}
+</script>
